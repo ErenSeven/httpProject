e69f55dfcffae026fe1ad5ffd933f1d6ec5ed99d
erenseven <20060356@stu.omu.edu.tr>
2024-05-27 20:24:45 +0300

    normalizeURL and test

---
+++ crawl.test.js
@@ -0,0 +1,31 @@
+const {normalizeURL} = require('./crawl.js')
+const {test, expect} = require('@jest/globals')
+
+
+test('mormalizeURL strip protocol', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('mormalizeURL strip trailing slash', () => {
+    const input = 'https://blog.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('mormalizeURL capitals', () => {
+    const input = 'https://BLOG.boot.dev/path/'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
+
+test('mormalizeURL strip HTTP', () => {
+    const input = 'https://blog.boot.dev/path'
+    const actual = normalizeURL(input)
+    const expected = 'blog.boot.dev/path'
+    expect(actual).toEqual(expected)
+})
